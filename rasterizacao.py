8a11f22b030811697ddd705976074b5af5bca419
edilson14 <edytavares66@gmail.com>
2023-03-29 07:04:26 -0300

    atualizacao do readme

--- rasterizacao.py
+++ rasterizacao.py
@@ -9,7 +9,7 @@ class Ponto:
 
 
 def calula_delta(ponto_final: int, ponto_inicial: int) -> float:
-    return math.fabs(ponto_final - ponto_inicial)
+    return (ponto_final - ponto_inicial)
 
 
 def produz_fragmento(x: float, y: float) -> list:
@@ -24,47 +24,55 @@ def rasterizacao_delta_x_maior_delta_y(ponto1: Ponto, m, b, x2) -> list:
     pontos: list = []
     pontos.append(produz_fragmento(ponto1.x, ponto1.y))
     while (ponto1.x < x2):
-        ponto1.x = ponto1.x+1
+        ponto1.x += 1
         ponto1.y = m*ponto1.x + b
         pontos.append(produz_fragmento(ponto1.x, ponto1.y))
     return pontos
 
 
 def rasterizacao_delta_y_maior_delta_x(ponto1: Ponto, m, b, ponto2: Ponto) -> list:
-    pontos: list = []
-    pontos.append(produz_fragmento(ponto1.x, ponto1.y))
-    while (ponto1.y < ponto2.y):
-        ponto1.y += 1
-        if (m != 0):
-            ponto1.x = (ponto1.y-b)/m
-        else:
-            ponto1.x = ponto2.x
-        pontos.append(produz_fragmento(ponto1.x, ponto1.y))
+    x1, x2, y1, y2 = ponto1.x, ponto2.x, ponto1.y, ponto2.y
 
+    pontos: list = []
+    pontos.append(produz_fragmento(x1, y1))
+    while (y1 < y2):
+        y1 += 1
+        if (m != 0):
+            x1 = (y1-b)/m
+        else:
+            x1 = x2
+        pontos.append(produz_fragmento(x1, y1))
     return pontos
 
 
 def rasterizacao_de_retas(ponto1: Ponto, ponto2: Ponto) -> None:
+    primeiro_ponto, segundo_ponto = ponto1, ponto2
     pontos: list
     # lista dos pontos para serem exibidas no grafico
     eixo_x: list = []
     eixo_y: list = []
+    if (ponto2.y < ponto1.y or ponto2.x < ponto1.y):
+        primeiro_ponto, segundo_ponto = segundo_ponto, primeiro_ponto
 
-    x = ponto1.x
-    y = ponto1.y
-    delta_x = calula_delta(ponto2.x, ponto1.x)
-    delta_y = calula_delta(ponto2.y, ponto1.y)
+    x = primeiro_ponto.x
+    y = primeiro_ponto.y
+    delta_x = calula_delta(segundo_ponto.x, primeiro_ponto.x)
+    delta_y = calula_delta(segundo_ponto.y, primeiro_ponto.y)
     if (delta_x != 0):
         m = delta_y/delta_x
     else:
-        m = 0
+        m = 0  # como o x não varia , a reta não possui inclinação
 
     if (abs(delta_x) > abs(delta_y)):
         b = (y - m)*x
-        pontos = rasterizacao_delta_x_maior_delta_y(ponto1, m, b, ponto2.x)
+        pontos = rasterizacao_delta_x_maior_delta_y(
+            primeiro_ponto, m, b, segundo_ponto.x)
     else:
         b = (y-m)*x
-        pontos = rasterizacao_delta_y_maior_delta_x(ponto1, m, b, ponto2)
+        pontos = rasterizacao_delta_y_maior_delta_x(
+            primeiro_ponto, m, b, segundo_ponto)
+    print(pontos)
+    # pontos.reverse()
     print(pontos)
     for ponto in pontos:
         eixo_x.append(ponto[0])
@@ -75,7 +83,7 @@ def rasterizacao_de_retas(ponto1: Ponto, ponto2: Ponto) -> None:
     plt.show()
 
 
-primeiro_ponto = Ponto(9, 1)
-segundo_ponto = Ponto(1, 3)
+primeiro_ponto = Ponto(9, 3)
+segundo_ponto = Ponto(0, 3)
 
 rasterizacao_de_retas(primeiro_ponto, segundo_ponto)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# CheckList das atividades
+- [x] Implementação do algoritmo apresentado na aula -  Feito
+- [x] Adaptação para variação de y > x - Feito
+- [x] Adaptação para as retas horizontais e verticais - Kildary 28/03 - 31/03 
+- [x] Adaptação para os pontos de incio serem maiores que pontos de fim - Edilson 01/04 - 09/04
+- [ ] Visualização das Imagens - Edilson - 28/03 31/03
+- [ ] Implementação do algoritmo scanLine para polignos - Kildary
+- [ ] Relatório de trabalho - Kildary e Edilson 28/03 - 10/04
