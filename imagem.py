c1585c56814b6c3e052e4a3970f072715b928004
edilson14 <edytavares66@gmail.com>
2023-04-16 13:01:29 -0300

    rasterizacao completa de linhas e polignos

--- imagem.py
+++ imagem.py
@@ -1,23 +1,27 @@
 import numpy as np
 
 
+class Resolucao:
+    def __init__(self, largura: int, altura: int) -> None:
+        self.largura = largura
+        self.altura = altura
+
+
 class Imagem:
     def __init__(self) -> None:
         pass
 
-    def criar_imagem_geometrica(todos_os_pontos, resolucao) -> any:
-        imag = np.zeros((resolucao[1], resolucao[0], 3), dtype=np.uint8)
+    def criar_imagem_geometrica(imagem, todos_os_pontos) -> any:
         for pontos in todos_os_pontos:
             eixo_x = []
             eixo_y = []
             for ponto in pontos:
                 eixo_x.append(int(ponto[0]))
                 eixo_y.append(int(ponto[1]))
-            imag[eixo_y, eixo_x] = [255, 0, 0]
-        return imag
+            imagem[eixo_y, eixo_x] = [255, 0, 0]
+        return imagem
 
-    def criar_imagem_reta(pontos, resolucao) -> any:
-        imag = np.zeros((resolucao[0], resolucao[1], 3), dtype=np.uint8)
+    def criar_imagem_reta(imag, pontos) -> any:
         eixo_x = []
         eixo_y = []
         for ponto in pontos:
--- rasterizacao.py
+++ rasterizacao.py
@@ -4,24 +4,27 @@ import numpy as np
 import imagem
 
 
+class Resolucao:
+    def __init__(self, largura: int, altura: int) -> None:
+        self.largura = largura
+        self.altura = altura
+
+
 largura, altura = 100, 100
-resolucao1 = largura, altura
-resolucao2 = largura*3, altura*3
-resolucao3 = largura*6, altura*6
-resolucao4 = largura*8, altura*6
-resolucao5 = int(altura*19.2), int(altura*10.8)
+resolucao1 = Resolucao(largura, altura)
+resolucao2 = Resolucao(largura*3, altura*3)
+resolucao3 = Resolucao(largura*6, altura*6)
+resolucao4 = Resolucao(largura*8, altura*6)
+resolucao5 = Resolucao(int(altura*19.2), int(altura*10.8))
 
 
 class Ponto:
-    def __init__(self, x: int, y: int, resolucao: list) -> None:
-        self.x, self.y = self.scale_point(x, y, resolucao[0], resolucao[1])
+    def __init__(self, x: float, y: float, resolucao: Resolucao) -> None:
+        self.x, self.y = self.scale_point(x, y, resolucao)
 
-    def scale_point(self, x, y, largura, altura):
-        x_dist = 1 - (-1)
-        # Distância entre as coordenadas máxima e mínima para y
-        y_dist = 1 - (-1)
-        scaled_x = ((x / x_dist) + 0.5) * largura
-        scaled_y = (1 - ((y / y_dist) + 0.5)) * altura
+    def scale_point(self, x, y, resolucao: Resolucao):
+        scaled_x = ((resolucao.largura - 1) * (x + 1)) / 2
+        scaled_y = ((resolucao.altura - 1) * (y + 1)) / 2
         return [round(scaled_x), round(scaled_y)]
 
 
@@ -42,14 +45,14 @@ def rasterizacao_delta_x_maior_delta_y(ponto1: Ponto, m, b, x2) -> list:
     pontos: list = []
     pontos.append(produz_fragmento(x1, y1))
     if (x1 < x2):
-        while (x1 <= x2):
+        while (x1 < x2):
             x1 = x1 + 1
             if (m != 0):
                 y1 = math.ceil(m*x1 + b)
             pontos.append(produz_fragmento(x1, y1))
     else:
         aux = x1
-        while (x2 <= aux):
+        while (x2 < aux):
             x2 = x2 + 1
             x1 = x1-1
             if (m != 0):
@@ -63,14 +66,14 @@ def rasterizacao_delta_y_maior_delta_x(ponto1: Ponto, m, b, ponto2: Ponto) -> li
 
     pontos: list = []
     if (y1 < y2):
-        while (y1 <= y2):
+        while (y1 < y2):
             pontos.append(produz_fragmento(x1, y1))
             y1 = y1 + 1
             if (m != 0):
                 x1 = ((y1-b)/m)
     else:
         aux = y1
-        while y2 <= aux:
+        while y2 < aux:
             pontos.append(produz_fragmento(x1, y1))
             y2 = y2 + 1
             y1 = y1 - 1
@@ -106,7 +109,7 @@ def rasterizacao_de_retas(ponto1: Ponto, ponto2: Ponto) -> None:
     return pontos
 
 
-def triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao):
+def triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao: Resolucao):
     pontos = []
     tri_ponto1 = Ponto(tri_ponto1[0], tri_ponto1[1], resolucao)
     tri_ponto2 = Ponto(tri_ponto2[0], tri_ponto2[1], resolucao)
@@ -117,119 +120,392 @@ def triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao):
     return pontos
 
 
-tri_ponto1 = (-0.5, -0.87)
-tri_ponto2 = (0.5, -0.87)
-tri_ponto3 = (0, 0.87)
-
-
-def triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao):
+def hexagono(hex1, hex2, hex3, hex4, hex5, hex6, resolucao):
     pontos = []
-    tri_ponto1 = Ponto(tri_ponto1[0], tri_ponto1[1], resolucao)
-    tri_ponto2 = Ponto(tri_ponto2[0], tri_ponto2[1], resolucao)
-    tri_ponto3 = Ponto(tri_ponto3[0], tri_ponto3[1], resolucao)
-    pontos.append(rasterizacao_de_retas(tri_ponto1, tri_ponto2))
-    pontos.append(rasterizacao_de_retas(tri_ponto2, tri_ponto3))
-    pontos.append(rasterizacao_de_retas(tri_ponto3, tri_ponto1))
+    hex1 = Ponto(hex1[0], hex1[1], resolucao)
+    hex2 = Ponto(hex2[0], hex2[1], resolucao)
+    hex3 = Ponto(hex3[0], hex3[1], resolucao)
+    hex4 = Ponto(hex4[0], hex4[1], resolucao)
+    hex5 = Ponto(hex5[0], hex5[1], resolucao)
+    hex6 = Ponto(hex6[0], hex6[1], resolucao)
+
+    pontos.append(rasterizacao_de_retas(hex1, hex2))
+    pontos.append(rasterizacao_de_retas(hex2, hex3))
+    pontos.append(rasterizacao_de_retas(hex3, hex4))
+    pontos.append(rasterizacao_de_retas(hex4, hex5))
+    pontos.append(rasterizacao_de_retas(hex5, hex6))
+    pontos.append(rasterizacao_de_retas(hex6, hex1))
+
     return pontos
 
 
-def rasteriza_poligno(arestas, largura, altura):
-    img_face_orig = np.zeros((largura, altura, 3), dtype="uint8")
-    img_face = np.zeros((largura, altura, 3), dtype="uint8")
+def quadrado(hex1, hex2, hex3, hex4, resolucao: Resolucao):
+    pontos = []
+    hex1 = Ponto(hex1[0], hex1[1], resolucao)
+    hex2 = Ponto(hex2[0], hex2[1], resolucao)
+    hex3 = Ponto(hex3[0], hex3[1], resolucao)
+    hex4 = Ponto(hex4[0], hex4[1], resolucao)
 
-    for aresta in arestas:
-        for ponto in aresta:
-            img_face_orig[ponto[0], ponto[1]] = [1, 1, 1]
+    pontos.append(rasterizacao_de_retas(hex1, hex2))
+    pontos.append(rasterizacao_de_retas(hex2, hex3))
+    pontos.append(rasterizacao_de_retas(hex3, hex4))
+    pontos.append(rasterizacao_de_retas(hex4, hex1))
 
-    for i in range(altura):
-        cont_h = 0
-        sava_ponto_h = []
-        cont_v = 0
-        sava_ponto_v = []
-        for j in range(largura - 1):
-            # Horizontal
-            if np.array_equal(img_face_orig[j, i], [1, 1, 1]):
-                cont_h += 1
-                if len(sava_ponto_h) > 1:
-                    del sava_ponto_h[0]
-                sava_ponto_h.append([j, i])
-            if cont_h == 2:
-                variacao_h = abs(sava_ponto_h[1][1] - sava_ponto_h[0][1])
-                for w in range(variacao_h):
-                    img_face[i, w + sava_ponto_h[0][1]] = [255, 255, 255]
-                cont_h = 1
-
-            # Vertical
-            if np.array_equal(img_face_orig[j, i], [1, 1, 1]):
-                cont_v += 1
-                if len(sava_ponto_v) > 1:
-                    del sava_ponto_v[0]
-                sava_ponto_v.append([j, i])
-            if cont_v == 2:
-                variacao_v = sava_ponto_v[1][0] - sava_ponto_v[0][0]
-                for z in range(variacao_v):
-                    img_face[z + sava_ponto_v[0][0], i] = [255, 255, 255]
-                cont_v = 1
-
-    arestas_rasterizadas = []
-    for i in range(altura):
-        for j in range(largura-1):
-            if np.array_equal(img_face[j, i], img_face_orig[j, i]):
-                continue
-
-            if np.array_equal(img_face[j, i], [255, 255, 255]):
-                arestas_rasterizadas.append([j, i])
-
-    return arestas_rasterizadas
+    return pontos
 
 
-pontosTriangulo1 = triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao3)
-Image1 = imagem.Imagem.criar_imagem_geometrica(pontosTriangulo1, resolucao3)
+def rasteriza_poligno(arestas, resolucao: Resolucao):
+    # Inicializa a imagem como uma matriz de zeros
+    imagem = np.zeros((resolucao.altura, resolucao.largura, 3), dtype=np.uint8)
+    pontos_internos = []
 
-pontos_internos = rasteriza_poligno(
-    pontosTriangulo1, resolucao3[0], resolucao3[1])
+    x = []
+    y = []
+    for pontos in arestas:
+        for ponto in pontos:
+            x.append((ponto[0]))
+            y.append((ponto[1]))
+        imagem[y, x] = [1, 1, 1]
+    # Para cada linha da imagem
+    for alt in range(resolucao.altura):
+        # Inicializa o número de interseções com zero
+        count = 0
+        # Para cada aresta do polígono
+        ponto_aux = []
+        for lar in range(resolucao.largura):
+            if (np.array_equal(imagem[alt, lar], [1, 1, 1])):
+                if (alt < resolucao.altura and (lar+1) < resolucao.largura and np.array_equal(imagem[alt, lar+1], [1, 1, 1])):
+                    # if ():
+                    pass
+                else:
+                    count += 1
+            if (count > 0 and count < 2):
+                ponto_aux.append([alt, lar])
+        if (len(ponto_aux) != 0 and count % 2 == 0):
+            pontos_internos.append(ponto_aux)
 
+    return pontos_internos
+
+
+# TRIANGULO INFERIOR
+tri_ponto1 = (-1, -1)
+tri_ponto2 = (-0.75, -0.50)
+tri_ponto3 = (-0.50, -1)
+
+# triangulo superior
+tri2_ponto1 = (-0.25, -0.5)
+tri2_ponto2 = (0.25, -0.5)
+tri2_ponto3 = (0, 0)
+# RETA
+ponto1 = (-0.75, -0.25)
+ponto2 = (-0.25, 0.25)
+
+# Reta horizontal
+ponto_reta2 = (-1, -0.5)
+ponto2_reta2 = (-0.5, 0.25)
+# Reta vertical
+ponto2_reta_v = (-0.30, -1)
+ponto2_reta_v = (-0.30, -0.5)
+
+
+# hexagono superior
+hex_ponto1 = (-0.25, 0.5)
+hex_ponto2 = (0, 0.25)
+hex_ponto3 = (0.25, 0.25)
+hex_ponto4 = (0.5, 0.5)
+hex_ponto5 = (0.25, 0.75)
+hex_ponto6 = (0, 0.75)
+
+# hexagono inferior
+hex2_ponto1 = (0.25, 0)
+hex2_ponto2 = (0.50, -0.25)
+hex2_ponto3 = (0.75, -0.25)
+hex2_ponto4 = (1, 0)
+hex2_ponto5 = (0.75, 0.25)
+hex2_ponto6 = (0.5, 0.25)
+
+
+quadr_p1 = (-1, 1)
+quadr_p2 = (-0.5, 1)
+quadr_p3 = (-0.5, 0.5)
+quadr_p4 = (-1, 0.5)
+
+# quadrado inferior
+quadr2_p1 = (0, -1)
+quadr2_p2 = (0.25, -1)
+quadr2_p3 = (0.25, -0.75)
+quadr2_p4 = (0, -0.75)
+
+
+# Imagem na resolução 100 x 100
+Image1 = np.zeros((resolucao1.altura, resolucao1.largura, 3), dtype=np.uint8)
+# triangulo inferior
+pontosTriangulo = triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao1)
+Image1 = imagem.Imagem.criar_imagem_geometrica(Image1, pontosTriangulo)
+pontos_internos = rasteriza_poligno(pontosTriangulo, resolucao1)
 for ponto in pontos_internos:
-    Image1[ponto[1], ponto[0]] = [255, 0, 0]
+    for p in ponto:
+        Image1[p[0], p[1]] = [255, 0, 0]
 
+# RETA
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-0.75, -0.25, resolucao1), Ponto(-0.25, 0.25, resolucao1))
+Image1 = imagem.Imagem.criar_imagem_reta(Image1, pontos_reta)
 
-pontosTriangulo2 = triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao4)
-Image2 = imagem.Imagem.criar_imagem_geometrica(pontosTriangulo2, resolucao4)
+# RETA HOZONTAL
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-1, 0.25, resolucao1), Ponto(-0.5, 0.25, resolucao1))
+Image1 = imagem.Imagem.criar_imagem_reta(Image1, pontos_reta)
 
-pontos_internos = rasteriza_poligno(
-    pontosTriangulo2, resolucao4[0], resolucao4[1])
+# RETA VERTICAL
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-0.3, -1, resolucao1), Ponto(-0.3, -0.5, resolucao1))
+Image1 = imagem.Imagem.criar_imagem_reta(Image1, pontos_reta)
 
+# hexagono superior
+pontoshexagono = hexagono(hex_ponto1, hex_ponto2, hex_ponto3,
+                          hex_ponto4, hex_ponto5, hex_ponto6, resolucao1)
+Image1 = imagem.Imagem.criar_imagem_geometrica(Image1, pontoshexagono)
+pontos_internos = rasteriza_poligno(pontoshexagono, resolucao1)
 for ponto in pontos_internos:
-    Image2[ponto[1], ponto[0]] = [255, 0, 0]
+    for p in ponto:
+        Image1[p[0], p[1]] = [255, 0, 0]
 
-pontosTriangulo3 = triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao5)
-Image3 = imagem.Imagem.criar_imagem_geometrica(pontosTriangulo3, resolucao5)
-
-pontos_internos = rasteriza_poligno(
-    pontosTriangulo3, resolucao5[0], resolucao5[1])
-
+# hexagono inferior
+pontos2hexagono = hexagono(hex2_ponto1, hex2_ponto2, hex2_ponto3,
+                           hex2_ponto4, hex2_ponto5, hex2_ponto6, resolucao1)
+Image1 = imagem.Imagem.criar_imagem_geometrica(Image1, pontos2hexagono)
+pontos_internos = rasteriza_poligno(pontos2hexagono, resolucao1)
 for ponto in pontos_internos:
-    Image3[ponto[1], ponto[0]] = [255, 0, 0]
+    for p in ponto:
+        Image1[p[0], p[1]] = [255, 0, 0]
+
+# triangulo
+pontosTriangulo2 = triangulo(tri2_ponto1, tri2_ponto2, tri2_ponto3, resolucao1)
+Image1 = imagem.Imagem.criar_imagem_geometrica(Image1, pontosTriangulo2)
+pontos_internos = rasteriza_poligno(pontosTriangulo2, resolucao1)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image1[p[0], p[1]] = [255, 0, 0]
 
 
-fig = plt.figure(figsize=(10, 7))
+# quadrado_superior
+pontos_quadrado = quadrado(quadr_p1, quadr_p2, quadr_p3, quadr_p4, resolucao1)
+Image1 = imagem.Imagem.criar_imagem_geometrica(Image1, pontos_quadrado)
+pontos_internos = rasteriza_poligno(pontos_quadrado, resolucao1)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image1[p[0], p[1]] = [255, 0, 0]
+
+# quadrado_inferior
+pontos_quadrado = quadrado(
+    quadr2_p1, quadr2_p2, quadr2_p3, quadr2_p4, resolucao1)
+Image1 = imagem.Imagem.criar_imagem_geometrica(Image1, pontos_quadrado)
+pontos_internos = rasteriza_poligno(pontos_quadrado, resolucao1)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image1[p[0], p[1]] = [255, 0, 0]
+
+# FIM DO TRIANGULO
+
+
+# Imagem na resolução 800 x 600
+Image2 = np.zeros((resolucao4.altura, resolucao4.largura, 3), dtype=np.uint8)
+# triangulo
+pontosTriangulo = triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao4)
+Image2 = imagem.Imagem.criar_imagem_geometrica(Image2, pontosTriangulo)
+pontos_internos = rasteriza_poligno(pontosTriangulo, resolucao4)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image2[p[0], p[1]] = [255, 0, 0]
+
+# FIM DO TRIANGULO
+
+# RETA
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-0.75, -0.25, resolucao4), Ponto(-0.25, 0.25, resolucao4))
+Image2 = imagem.Imagem.criar_imagem_reta(Image2, pontos_reta)
+
+
+# RETA HOZONTAL
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-1, 0.25, resolucao4), Ponto(-0.5, 0.25, resolucao4))
+Image2 = imagem.Imagem.criar_imagem_reta(Image2, pontos_reta)
+
+# RETA VERTICAL
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-0.3, -1, resolucao4), Ponto(-0.3, -0.5, resolucao4))
+Image2 = imagem.Imagem.criar_imagem_reta(Image2, pontos_reta)
+
+# hexagono superior
+pontoshexagono = hexagono(hex_ponto1, hex_ponto2, hex_ponto3,
+                          hex_ponto4, hex_ponto5, hex_ponto6, resolucao4)
+Image2 = imagem.Imagem.criar_imagem_geometrica(Image2, pontoshexagono)
+pontos_internos = rasteriza_poligno(pontoshexagono, resolucao4)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image2[p[0], p[1]] = [255, 0, 0]
+
+# hexagono inferior
+pontoshexa2gono = hexagono(hex2_ponto1, hex2_ponto2, hex2_ponto3,
+                           hex2_ponto4, hex2_ponto5, hex2_ponto6, resolucao4)
+Image2 = imagem.Imagem.criar_imagem_geometrica(Image2, pontoshexa2gono)
+pontos_internos = rasteriza_poligno(pontoshexa2gono, resolucao4)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image2[p[0], p[1]] = [255, 0, 0]
+
+pontosTriangulo2 = triangulo(tri2_ponto1, tri2_ponto2, tri2_ponto3, resolucao4)
+Image2 = imagem.Imagem.criar_imagem_geometrica(Image2, pontosTriangulo2)
+pontos_internos = rasteriza_poligno(pontosTriangulo2, resolucao4)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image2[p[0], p[1]] = [255, 0, 0]
+
+pontos_quadrado = quadrado(quadr_p1, quadr_p2, quadr_p3, quadr_p4, resolucao4)
+Image2 = imagem.Imagem.criar_imagem_geometrica(Image2, pontos_quadrado)
+pontos_internos = rasteriza_poligno(pontos_quadrado, resolucao4)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image2[p[0], p[1]] = [255, 0, 0]
+
+# quadrado_inferior
+pontos_quadrado = quadrado(
+    quadr2_p1, quadr2_p2, quadr2_p3, quadr2_p4, resolucao4)
+Image2 = imagem.Imagem.criar_imagem_geometrica(Image2, pontos_quadrado)
+pontos_internos = rasteriza_poligno(pontos_quadrado, resolucao4)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image2[p[0], p[1]] = [255, 0, 0]
+
+
+# RESOLUÇÃO 1920X1080
+Image3 = np.zeros((resolucao5.altura, resolucao5.largura, 3), dtype=np.uint8)
+# triangulo
+pontosTriangulo = triangulo(tri_ponto1, tri_ponto2, tri_ponto3, resolucao5)
+Image3 = imagem.Imagem.criar_imagem_geometrica(Image3, pontosTriangulo)
+pontos_internos = rasteriza_poligno(pontosTriangulo, resolucao5)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image3[p[0], p[1]] = [255, 0, 0]
+
+# FIM DO TRIANGULO
+
+# RETA
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-0.75, -0.25, resolucao5), Ponto(-0.25, 0.25, resolucao5))
+Image3 = imagem.Imagem.criar_imagem_reta(Image3, pontos_reta)
+
+# RETA HOZONTAL
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-1, 0.25, resolucao5), Ponto(-0.5, 0.25, resolucao5))
+Image3 = imagem.Imagem.criar_imagem_reta(Image3, pontos_reta)
+
+# RETA VERTICAL
+pontos_reta = rasterizacao_de_retas(
+    Ponto(-0.3, -1, resolucao5), Ponto(-0.3, -0.5, resolucao5))
+Image3 = imagem.Imagem.criar_imagem_reta(Image3, pontos_reta)
+
+# hexagono superior
+pontoshexagono = hexagono(hex_ponto1, hex_ponto2, hex_ponto3,
+                          hex_ponto4, hex_ponto5, hex_ponto6, resolucao5)
+Image3 = imagem.Imagem.criar_imagem_geometrica(Image3, pontoshexagono)
+pontos_internos = rasteriza_poligno(pontoshexagono, resolucao5)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image3[p[0], p[1]] = [255, 0, 0]
+
+# hexagono inferior
+pontoshexagono2 = hexagono(hex2_ponto1, hex2_ponto2, hex2_ponto3,
+                           hex2_ponto4, hex2_ponto5, hex2_ponto6, resolucao5)
+Image3 = imagem.Imagem.criar_imagem_geometrica(Image3, pontoshexagono2)
+pontos_internos = rasteriza_poligno(pontoshexagono2, resolucao5)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image3[p[0], p[1]] = [255, 0, 0]
+
+pontosTriangulo2 = triangulo(tri2_ponto1, tri2_ponto2, tri2_ponto3, resolucao5)
+Image3 = imagem.Imagem.criar_imagem_geometrica(Image3, pontosTriangulo2)
+pontos_internos = rasteriza_poligno(pontosTriangulo2, resolucao5)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image3[p[0], p[1]] = [255, 0, 0]
+
+pontos_quadrado = quadrado(quadr_p1, quadr_p2, quadr_p3, quadr_p4, resolucao5)
+Image3 = imagem.Imagem.criar_imagem_geometrica(Image3, pontos_quadrado)
+pontos_internos = rasteriza_poligno(pontos_quadrado, resolucao5)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image3[p[0], p[1]] = [255, 0, 0]
+
+# quadrado_inferior
+pontos_quadrado = quadrado(
+    quadr2_p1, quadr2_p2, quadr2_p3, quadr2_p4, resolucao5)
+Image3 = imagem.Imagem.criar_imagem_geometrica(Image3, pontos_quadrado)
+pontos_internos = rasteriza_poligno(pontos_quadrado, resolucao5)
+for ponto in pontos_internos:
+    for p in ponto:
+        Image3[p[0], p[1]] = [255, 0, 0]
+
+
+fig = plt.figure(figsize=(10, 10))
 rows = 1
-columns = 3
-
+columns = 4
 
 fig.add_subplot(rows, columns, 1)
-plt.imshow(Image1)
-plt.axis('off')
-plt.title("100x100")
+# triangulo azul
+x = [-1, -0.75, -0.50, -1]
+y = [-1, -0.50, -1, -1]
+plt.plot(x, y)
+
+# reta inclinada laranja
+x1 = [-0.75, -0.25]
+y1 = [-0.25, 0.25]
+plt.plot(x1, y1)
+# reta verde horizontal
+x2 = [-1, -0.5]
+y2 = [0.25, 0.25]
+plt.plot(x2, y2)
+# reta vermelha vertical
+x3 = [-0.30, -0.30]
+y3 = [-1, -0.50]
+plt.plot(x3, y3)
+# quadrado roxo maior
+x5 = [-1, -0.50, -0.50, -1, -1]
+y5 = [1, 1, 0.50, 0.50, 1]
+plt.plot(x5, y5)
+# quadrado castanho menor
+x7 = [0, 0.25, 0.25, 0, 0]
+y7 = [-1, -1, -0.75, -0.75, -1]
+plt.plot(x7, y7)
+
+# hexagono superior
+x8 = [-0.25,  0,  0.25, 0.5, 0.25, 0, -0.25]
+y8 = [0.5, 0.25, 0.25, 0.5, 0.75, 0.75, 0.5]
+plt.plot(x8, y8)
+# triangulo cinza
+x9 = [-0.25, 0.25, 0, -0.25]
+y9 = [-0.5, -0.5, 0, -0.5]
+plt.plot(x9, y9)
+# hexagono inferior
+x10 = [0.25, 0.50, 0.75, 1, 0.75, 0.5, 0.25]
+y10 = [0, -0.25, -0.25, 0, 0.25, 0.25, 0]
+plt.plot(x10, y10)
+plt.title('Figuras entre -1 e 1')
 
 fig.add_subplot(rows, columns, 2)
-plt.imshow(Image2)
-plt.axis('off')
-plt.title("300x300")
+plt.imshow(Image1)
+plt.gca().invert_yaxis()
+plt.title("100x100")
 
 fig.add_subplot(rows, columns, 3)
+plt.imshow(Image2)
+plt.gca().invert_yaxis()
+plt.title("800x600")
+
+fig.add_subplot(rows, columns, 4)
 plt.imshow(Image3)
-plt.axis('off')
+plt.gca().invert_yaxis()  # Inverte o eixo y
 plt.title("600x600")
 
 
